ab7400d1241b56f5a6e5371226d832a49cb25f95
Evan Harrison <esharri2@gmail.com>
2020-12-20 14:55:40 -0500

    save

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
-  variants: {},
-  plugins: [],
-};
